c6020b8d55920b6bba995473e6275f7585f019ff
Law Yang <lawtouayang@gmail.com>
2018-06-07 13:19:23 -0500

    params

--- server/routes/song.router.js
+++ server/routes/song.router.js
@@ -22,13 +22,37 @@ router.get('/', ( req, res ) => {
 
 router.post('/', ( req, res ) => {
     console.log(`in song router-POST`);
-    res.sendStatus(200);
+    // uses req.body
+    //INSERT INTO "songs"("id", "rank", "artist", "track", "published") VALUES(160, 412, 'Gordon Jenkins & The Weavers', 'Goodnight, Irene', '1/1/1950')
+    let queryPost = `INSERT INTO "songs"("rank", "artist", "track", "published") VALUES(412, 'Curtis  Law Band', 'Goodnight Sweet Princes', '1/1/1950');`; 
+    pool.query(queryPost)
+        .then((result) => {
+            res.send(result.rows);
+            console.log(`success POST of song ${result}`);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log(`Error POSTING: ${err}`);
+            res.sendStatus(500);
+        })
 })
 
-router.put('/', ( req, res ) => {
-    console.log(`in song router-UPDATE`);
-    res.sendStatus(200);
-})
+router.put('/:id', (req, res) => {
+    console.log('In song-router PUT to update', req.body);
+ 
+    const update = req.body;
+    const id = req.params.id;
+    let queryText = `UPDATE songs SET rank = $2, artist = $3 WHERE id=$1`;
+ 
+    pool.query(queryText, [id, update.rank, update.artist])
+        .then( (result) => {
+            console.log('back from DB with', result);
+            res.sendStatus(200);
+        }).catch( (err) => {
+            console.log('Error', err);
+            res.sendStatus(500);
+        });
+ });
 
 router.delete('/:id', ( req, res ) => {
     console.log(`in song router-DELETE`);
@@ -38,8 +62,8 @@ router.delete('/:id', ( req, res ) => {
     // 2) the calues to substitute into the query for the $1, $2, $3
     // when subbin in multiple things, the order is important
     pool.query(queryText, [id])
-        .then((results) => {
-            console.log(`successful delete of song ${results}`);
+        .then((result) => {
+            console.log(`successful delete of song ${result}`);
             res.sendStatus(200);
         })
         .catch((err) => {
@@ -47,7 +71,6 @@ router.delete('/:id', ( req, res ) => {
             res.sendStatus(500);
             
         })
-    res.sendStatus(200);
 })
 
 module.exports = router;
